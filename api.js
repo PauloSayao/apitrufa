fdbc177f9873defd36c159269b2e44c9b09676f7
PauloSayao <sayaopaulo@gmail.com>
2025-05-20 21:16:18 -0300

    Update api.js

--- api.js
+++ api.js
@@ -12,7 +12,16 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.options('*', cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://projeto-final-one-ruby.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 
 app.use(express.json());
 
