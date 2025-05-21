efbb5ba66d6e081aff2e043e9cf05ea53a202c83
PauloSayao <sayaopaulo@gmail.com>
2025-05-20 21:08:49 -0300

    Update api.js

--- api.js
+++ api.js
@@ -4,8 +4,14 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
+const corsOptions = {
+  origin: ['http://localhost:4200', 'https://projeto-final-one-ruby.vercel.app'],
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname)));
